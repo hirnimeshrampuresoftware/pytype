f003149871570513b51bb98dca61d30dc91a9aa9
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2021-08-27 12:03:59 +0530

    Update manylinux.sh

--- build_scripts/wheels/manylinux.sh
+++ build_scripts/wheels/manylinux.sh
@@ -30,7 +30,8 @@ mv ninja /usr/local/bin/
 rm -vf ninja*
 ln -s /usr/local/bin/ninja /usr/local/bin/ninja-build
 
-pip3 install scikit-build ninja
+pip3 install scikit-build
+pip3 install ninja
 
 TD="$(mktemp -d)"
 pushd "$TD" || exit 1
